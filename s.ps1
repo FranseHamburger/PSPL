a9b7130b0f3a99db70b30abd8da497a1473c38ea
FranseHamburger <102481435+FranseHamburger@users.noreply.github.com>
2022-03-28 20:17:54 +0200

    Update s.ps1

--- s.ps1
+++ s.ps1
@@ -1,1 +1,1 @@
-sleep -s 600 ; start chrome https://youtu.be/EApem1uUwpg ; del .\p.bat .\s.ps1
+sleep -s 600 ; start chrome https://www.youtube.com/watch?v=pxw-5qfJ1dk ; Remove-Item p.bat ; Remove-Item s.ps1
